3e6d1d75c26220c9a5b27a9bab1906dcf33128e8
zix <zixzixzix@126.com>
2019-03-31 22:45:52 +0800

    6.3.2

--- functional.js
+++ functional.js
@@ -18,7 +18,68 @@ function functionalUrls(elements){
 }
 console.log(functionalUrls(states));
 
-function functionalFullUrls(elements){
-    return elements.map(element => `https://example.com/${urlify(element)}`);
+// function functionalFullUrls(elements){
+//     return elements.map(element => `https://example.com/${urlify(element)}`);
+// }
+// console.log(functionalFullUrls(states));
+
+// singles: Imperative version
+function imperativeSingles(elements) {
+  let singles = [];
+  elements.forEach(function(element) {
+    if (element.split(/\s+/).length === 1) {
+      singles.push(element);
+    }
+  });
+  return singles;
 }
-console.log(functionalFullUrls(states));
+console.log(imperativeSingles(states));
+
+//singles: Functional version
+function functionalSingles(elements) {
+  return elements.filter(element => element.split(/\s+/).length === 1);
+}
+
+console.log(functionalSingles(states));
+
+function functionalDokotas(elements) {
+  // return elements.filter(element => element.split(/\s+/).length === 2);
+  return elements.filter(element => element.includes("Dakota"));
+}
+console.log(functionalDokotas(states));
+
+let numbers = [1,2,3,4,5,6,7,8,9,10];
+//sum: Imperative solution
+function imperativeSum(elements){
+  let total = 0;
+  elements.forEach(function(n){
+    total += n;
+  });
+  return total;
+}
+console.log(imperativeSum(numbers));
+
+//sum: Functional solution
+function functionalSum(elements) {
+  return elements.reduce((total,n) => {return total += n});
+}
+console.log(functionalSum(numbers));
+
+//length: Imperative solution
+function imperativeLengths(elements){
+  let lengths = {};
+  elements.forEach(function(element){
+    lengths[element] = element.length;
+  });
+  return  lengths;
+}
+console.log(imperativeLengths(states));
+
+//length: Functional solution
+function functionalLengths(elements){
+  return elements.reduce((lengths,element) => {
+                   lengths[element] = element.length;
+                   return lengths;
+                 },{});
+}
+console.log(functionalLengths(states));
