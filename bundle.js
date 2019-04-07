4ad2cb1d4c7d85a6535ba830fc150633654259a7
zix <zixzixzix@126.com>
2019-04-07 16:52:29 +0800

    9.4.1

--- bundle.js
+++ bundle.js
@@ -1,12 +1,23 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 let Phrase = require("zix-palindrome");
-let string = prompt("Please enter a string for palindrome testing:");
-let phrase = new Phrase(string);
-if(phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome!`);
-}else{
-  alert(`"${phrase.content}" is a not a palindrome!`);
+
+function palindromeTester(event){
+  event.preventDefault();
+  let string = event.target.phrase.value
+  let phrase = new Phrase(string);
+  if(phrase.palindrome()) {
+    palindromeResult.innerHTML = `<strong>"${phrase.content}"</strong> is a palindrome!`;
+  }else{
+    palindromeResult.innerHTML = `<strong>"${phrase.content}"</strong> is not a palindrome!`;
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function(){
+  let tester = document.querySelector("#palindromeTester");
+  tester.addEventListener("submit",function(event){
+    palindromeTester(event);
+  });
+});
 
 },{"zix-palindrome":2}],2:[function(require,module,exports){
 module.exports = Phrase;
@@ -25,13 +36,17 @@ function Phrase(content){
   }
   //Retruns the latters in the content.
   this.letters = function (){
-    // const letterRegex = /[a-z]/i;
+    const letterRegex = /[a-z]/gi;
     // return Array.from(this.content).filter(c => c.match(letterRegex)).join("");
-    return (this.content.match(/[a-z]/gi) || []).join("");
+    return (this.content.match(letterRegex) || []).join("");
   }
   // Retruns ture if the phrase is a palindrome, false otherwise.
   this.palindrome = function (){
-    return this.processedContent() === this.processedContent().reverse();
+    if(this.processedContent()){
+      return this.processedContent() === this.processedContent().reverse();
+    }else{
+      return false
+    }
   }
 }
 
--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 let Phrase = require("zix-palindrome");
-let string = prompt("Please enter a string for palindrome testing:");
-let phrase = new Phrase(string);
-if(phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome!`);
-}else{
-  alert(`"${phrase.content}" is a not a palindrome!`);
+
+function palindromeTester(event){
+  event.preventDefault();
+  let string = event.target.phrase.value
+  let phrase = new Phrase(string);
+  if(phrase.palindrome()) {
+    palindromeResult.innerHTML = `<strong>"${phrase.content}"</strong> is a palindrome!`;
+  }else{
+    palindromeResult.innerHTML = `<strong>"${phrase.content}"</strong> is not a palindrome!`;
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function(){
+  let tester = document.querySelector("#palindromeTester");
+  tester.addEventListener("submit",function(event){
+    palindromeTester(event);
+  });
+});
